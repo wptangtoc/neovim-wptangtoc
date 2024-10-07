bc0551a8f0891a811117bc41d242cb76f8032b8b
GiaTuan <giatuan@wptangtoc.com>
2024-10-07 17:07:52 +0700

    neovim

--- neovim-wptangtoc.sh
+++ neovim-wptangtoc.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if $(cat /etc/*release | grep -q "Ubuntu") ; then
-	sudo apt install ripgrep fzf neovim nodejs -y
+	sudo apt install ripgrep fzf neovim nodejs git -y
 else
 	sudo dnf install epel-release -y
-	sudo dnf install ripgrep fzf neovim nodejs -y
+	sudo dnf install ripgrep fzf neovim nodejs git -y
 fi
 cd
 wget https://github.com/wptangtoc/neovim-wptangtoc/raw/refs/heads/main/neovim.zip
@@ -15,6 +15,10 @@ rm -f neovim-wptangtoc.sh
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 rm -f neovim.zip
 ln -f /usr/bin/nvim /usr/bin/vim
-echo 'Hướng dẫn Cài đặt install neovim'
-echo "trên terminal bạn hãy gõ lệnh: vim . rồi sau đó gõ :PlugInstall"
-echo "rồi tiếp tục gõ :CocInstall coc-phpls coc-css coc-html"
+clear
+echo '====================================='
+echo 'hoàn tất cài đặt neovim'
+echo "trên terminal bạn hãy gõ lệnh: vim ."
+echo "rồi sau đó gõ=> :PlugInstall"
+echo "rồi tiếp tục gõ=> :CocInstall coc-phpls coc-css coc-html"
+echo "Để thoát ra gõ=> :q!"
